388b006dadd34a740dad02271aab24e8bd9a71e9
Antoni Przybylik <antoni.przybylik@wp.pl>
2023-01-12 16:46:09 +0100

    Added more comments, translated some to Polish.

--- halma.py
+++ halma.py
@@ -2,34 +2,50 @@ from enum import Enum
 
 
 class ModeError(Exception):
+    """! Wyjątek rzucany gdy gra nie wspiera rządanego trybu. """
+
     def __init__(self, obj):
         super().__init__('Unknown or invalid mode.')
 
 
 class state(Enum):
+    """! Stan pola planszy. """
     EMPTY = 1
     WHITE = 2
     BLACK = 3
 
 
 class game:
-    """
-    supported_modes
+    """! Reprezentuje grę Halma.
 
-    setup()
-    moves()
-    print()
+    Parametry publiczne:
+        supported_modes
+        mode
+        move
+
+    Metody publiczne:
+        setup()
+        moves()
+        get_board()
+        set_field()
+        read_field()
     """
+
     def __init__(self):
+        """! Konstruktor klasy game. """
         self._board = [[state.EMPTY]*16 for i in range(16)]
-        self.supported_modes = ['classic']
 
+        self.supported_modes = ['classic']
         self.mode = None
         self.move = 1  # Obecny ruch.
 
     def setup(self, mode):
-        """
-        Setups the game.
+        """! Ustawia grę.
+
+        Tą funkcję trzeba wykonać przed
+        rozpoczęciem gry.
+
+        @param mode Tryb gry.
         """
 
         if (type(mode) != str):
@@ -43,10 +59,9 @@ class game:
             return
 
     def _classic_mode_setup(self):
-        """
-        Setups game in classic mode.
-        This function is meant to be
-        used throught setup method.
+        """! Funkcja pomocnicza metody setup.
+
+        Ustawia grę w trybie klasycznym.
         """
 
         # Ustawiamy obóz Czarnego.
@@ -60,6 +75,13 @@ class game:
                 self._board[15 - i][15 - j] = state.WHITE
 
     def moves(self, x, y):
+        """! Znajduje wszystkie pola na które można wykonać ruch.
+
+        @param x Współrzędna X pola.
+        @param y Współrzędna Y pola.
+
+        @return Lista pól na które można wykonać ruch z danego pola.
+        """
         if (type(x) != int):
             raise TypeError('Invalid coordinates')
         if (type(x) != int):
@@ -68,6 +90,13 @@ class game:
         return self._moves(x, y)
 
     def _pos_on_board(self, x, y):
+        """! Sprawdza, czy pole jest na planszy.
+
+        @param x Współrzędna X pola.
+        @param y Współrzędna Y pola.
+
+        @return Czy pole jest na planszy.
+        """
         if (x < 0 or y < 0):
             return False
         if (x > 15 or y > 15):
@@ -75,17 +104,41 @@ class game:
         return True
 
     def _pos_permitted(self, x, y, forbidden):
+        """! Sprawdza, czy pole jest na liście pól zakazanych.
+
+        @param x Współrzędna X pola.
+        @param y Współrzędna Y pola.
+        @param forbidden Lista pól zakazanych.
+
+        @return Czy pole jest na liście pól zakazanych.
+        """
         if ((x, y) in forbidden):
             return False
         return True
 
     def _validate_pos(self, x, y, forbidden):
+        """! Sprawdza, czy wolno się ruszyć na dane pole.
+
+        @param x Współrzędna X pola.
+        @param y Współrzędna Y pola.
+        @param visited Lista pól zakazanych.
+
+        @return Czy wolno się ruszyć na dane pole.
+        """
         if (self._pos_on_board(x, y) and
             self._pos_permitted(x, y, forbidden)):  # noqa: E129
             return True
         return False
 
     def _moves(self, x, y, visited=[]):
+        """! Funkcja pomocnicza metody moves.
+
+        @param x Współrzędna X pola.
+        @param y Współrzędna Y pola.
+        @param visited Odwiedzone pola.
+
+        @return Pola na które można się ruszyć.
+        """
         delta_x = [-1, 0, 1, 1,  1,  0, -1, -1]
         delta_y = [ 1, 1, 1, 0, -1, -1, -1,  0]  # noqa: E201
 
@@ -113,15 +166,19 @@ class game:
         return possible
 
     def get_board(self):
-        """
-        Returns gameboard.
+        """! Returns gameboard.
+
+        @return Plansza.
         """
 
         return self._board
 
     def set_field(self, x, y, value):
-        """
-        Sets a field in some state.
+        """! Ustawia pole w danym stanie.
+
+        @param x Współrzędna X pola.
+        @param y Współrzędna Y pola.
+        @param value Stan pola.
         """
 
         if (not self._pos_on_board(x, y)):
@@ -133,8 +190,12 @@ class game:
         self._board[x][y] = value
 
     def read_field(self, x, y):
-        """
-        Reads value from some field.
+        """! Zwraca stan danego pola.
+
+        @param x Współrzędna X pola.
+        @param y Współrzędna Y pola.
+
+        @return Stan pola.
         """
 
         if (not self._pos_on_board(x, y)):
