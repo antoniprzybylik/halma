32a40d4f5dc611d5cff5db4b1644a40f7d6ecfca
Antoni Przybylik <antoni.przybylik@wp.pl>
2023-01-20 21:23:44 +0100

    Implemented interface tests.

--- tests/test_iface.py
+++ tests/test_iface.py
@@ -4,7 +4,7 @@
 #
 # Autor: Antoni Przybylik
 
-from halma.game import Game
+from halma.engine import Engine
 from halma.iface import GameInterface
 
 from halma.defs import STATE
@@ -17,43 +17,43 @@ from halma.defs import CAMP
 
 
 def test_in_camp1():
-    game = Game()
-    game_iface = GameInterface(game)
+    engine = Engine()
+    game_iface = GameInterface(engine)
 
     assert game_iface.in_camp(0, 0) == CAMP.BLACK
 
 
 def test_in_camp2():
-    game = Game()
-    game_iface = GameInterface(game)
+    engine = Engine()
+    game_iface = GameInterface(engine)
 
     assert game_iface.in_camp(1, 4) == CAMP.BLACK
 
 
 def test_in_camp3():
-    game = Game()
-    game_iface = GameInterface(game)
+    engine = Engine()
+    game_iface = GameInterface(engine)
 
     assert game_iface.in_camp(1, 5) is None
 
 
 def test_in_camp4():
-    game = Game()
-    game_iface = GameInterface(game)
+    engine = Engine()
+    game_iface = GameInterface(engine)
 
     assert game_iface.in_camp(15, 15) == CAMP.WHITE
 
 
 def test_in_camp5():
-    game = Game()
-    game_iface = GameInterface(game)
+    engine = Engine()
+    game_iface = GameInterface(engine)
 
     assert game_iface.in_camp(14, 14) == CAMP.WHITE
 
 
 def test_in_camp6():
-    game = Game()
-    game_iface = GameInterface(game)
+    engine = Engine()
+    game_iface = GameInterface(engine)
 
     assert game_iface.in_camp(8, 8) is None
 
@@ -64,8 +64,8 @@ def test_in_camp6():
 
 
 def test_setup_classic():
-    game = Game()
-    game_iface = GameInterface(game)
+    engine = Engine()
+    game_iface = GameInterface(engine)
     game_iface.setup('classic')
 
     board = game_iface.get_board()
@@ -91,68 +91,68 @@ def test_setup_classic():
 # Ta funkcja wykonuje ruch.
 
 def test_move1():
-    game = Game()
-    game_iface = GameInterface(game)
+    engine = Engine()
+    game_iface = GameInterface(engine)
 
-    game.set_field(1, 1, STATE.WHITE)
-    game.set_field(2, 1, STATE.BLACK)
+    engine.set_field(1, 1, STATE.WHITE)
+    engine.set_field(2, 1, STATE.BLACK)
 
     # Czy uda się wykonać ruch.
     assert game_iface.move('bb-cc') is True
 
 
 def test_move2():
-    game = Game()
-    game_iface = GameInterface(game)
+    engine = Engine()
+    game_iface = GameInterface(engine)
 
-    game.set_field(1, 1, STATE.WHITE)
-    game.set_field(2, 1, STATE.BLACK)
+    engine.set_field(1, 1, STATE.WHITE)
+    engine.set_field(2, 1, STATE.BLACK)
 
     # Czy uda się wykonać ruch.
     assert game_iface.move('bb-bc') is True
 
 
 def test_move3():
-    game = Game()
-    game_iface = GameInterface(game)
+    engine = Engine()
+    game_iface = GameInterface(engine)
 
-    game.set_field(1, 1, STATE.WHITE)
-    game.set_field(2, 1, STATE.BLACK)
+    engine.set_field(1, 1, STATE.WHITE)
+    engine.set_field(2, 1, STATE.BLACK)
 
     # Czy uda się wykonać ruch.
     assert game_iface.move('bb-cb') is False
 
 
 def test_move4():
-    game = Game()
-    game_iface = GameInterface(game)
+    engine = Engine()
+    game_iface = GameInterface(engine)
 
-    game.set_field(1, 1, STATE.WHITE)
-    game.set_field(2, 1, STATE.BLACK)
+    engine.set_field(1, 1, STATE.WHITE)
+    engine.set_field(2, 1, STATE.BLACK)
 
     # Czy uda się wykonać ruch.
     assert game_iface.move('bb-ac') is True
 
 
 def test_move5():
-    game = Game()
-    game_iface = GameInterface(game)
+    engine = Engine()
+    game_iface = GameInterface(engine)
 
-    game.set_field(1, 3, STATE.WHITE)
-    game.set_field(2, 5, STATE.BLACK)
-    game.set_field(3, 4, STATE.BLACK)
+    engine.set_field(1, 3, STATE.WHITE)
+    engine.set_field(2, 5, STATE.BLACK)
+    engine.set_field(3, 4, STATE.BLACK)
 
     # Czy uda się wykonać ruch.
     assert game_iface.move('bd-ce') is True
 
 
 def test_move6():
-    game = Game()
-    game_iface = GameInterface(game)
+    engine = Engine()
+    game_iface = GameInterface(engine)
 
-    game.set_field(1, 0, STATE.BLACK)
-    game.set_field(0, 3, STATE.BLACK)
-    game.set_field(1, 2, STATE.WHITE)
+    engine.set_field(1, 0, STATE.BLACK)
+    engine.set_field(0, 3, STATE.BLACK)
+    engine.set_field(1, 2, STATE.WHITE)
 
     # Czy uda się wykonać ruch.
     assert game_iface.move('bc-ac') is True
